01f2843afea11906444ae5baae0fae6551c8089b
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-10-11 13:57:59 -0400

    use project and resource routers

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express")
 
 const server = express()
 
+const projectsRouter = require("./api/projects/projectsRouter")
+const resourceRouter = require("./api/resources/resourceRouter")
+
 server.use(express.json())
 
 server.use("/api/projects", projectsRouter)
+server.use("/api/resources", resourceRouter)
 
 module.exports = server
