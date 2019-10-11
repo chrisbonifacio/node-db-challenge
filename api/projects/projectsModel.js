0e9dbc0a1caec52e94de2198b9554b6160c8f9d9
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-10-11 13:58:20 -0400

    create helper functions for project table

---
+++ api/projects/projectsModel.js
@@ -0,0 +1,37 @@
+const db = require("../../data/dbConfig")
+
+module.exports = {
+  get,
+  getById,
+  add,
+  addProjectTask,
+  getProjectTasks
+}
+
+function get() {
+  return db("projects")
+}
+
+function getById(id) {
+  return db("projects")
+    .select("*")
+    .where({ id })
+}
+
+async function add(project) {
+  const [id] = await db("projects").insert(project)
+
+  return db("projects")
+    .select("*")
+    .where({ id: id })
+}
+
+function addProjectTask(taskData) {
+  return db("tasks").insert(taskData)
+}
+
+function getProjectTasks(id) {
+  return db("tasks")
+    .select("*")
+    .where({ project_id: id })
+}
