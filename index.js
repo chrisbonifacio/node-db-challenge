7644884ed2d6265ad405f6caf389ffe121811260
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-10-11 13:57:44 -0400

    fix logging server message

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const server = require("./server")
 
 const PORT = 5000
 
-server.listen(PORT, () => `Listening on port ${PORT}...`)
+server.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
